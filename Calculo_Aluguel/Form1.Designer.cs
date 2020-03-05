daa381ab0773a7b9d070464cdcc9e040974ff0f7
Rodrigo carvalho barbosa <rodrigocarvalhobarbosa@hotmail.com>
2020-03-05 05:43:24 -0300

    Add project files.

---
+++ Calculo_Aluguel/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Calculo_Aluguel
+{
+    partial class calculoAluguel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDiretorio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.datasAluguel = new System.Windows.Forms.ListBox();
+            this.btnRelatorio = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ckCartao = new System.Windows.Forms.CheckBox();
+            this.lbl_nomeCliente = new System.Windows.Forms.Label();
+            this.melhorCarro = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblQtdPessoas = new System.Windows.Forms.Label();
+            this.lblDiasAluguel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Local do Arquivo do Cliente:";
+            // 
+            // txtDiretorio
+            // 
+            this.txtDiretorio.Location = new System.Drawing.Point(148, 6);
+            this.txtDiretorio.Name = "txtDiretorio";
+            this.txtDiretorio.Size = new System.Drawing.Size(266, 20);
+            this.txtDiretorio.TabIndex = 1;
+            this.txtDiretorio.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtDiretorio_MouseClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Qtd de Pessoas: ";
+            // 
+            // datasAluguel
+            // 
+            this.datasAluguel.FormattingEnabled = true;
+            this.datasAluguel.Location = new System.Drawing.Point(100, 110);
+            this.datasAluguel.Name = "datasAluguel";
+            this.datasAluguel.Size = new System.Drawing.Size(215, 69);
+            this.datasAluguel.TabIndex = 6;
+            // 
+            // btnRelatorio
+            // 
+            this.btnRelatorio.Location = new System.Drawing.Point(148, 261);
+            this.btnRelatorio.Name = "btnRelatorio";
+            this.btnRelatorio.Size = new System.Drawing.Size(123, 23);
+            this.btnRelatorio.TabIndex = 8;
+            this.btnRelatorio.Text = "Gerar Relatório";
+            this.btnRelatorio.UseVisualStyleBackColor = true;
+            this.btnRelatorio.Click += new System.EventHandler(this.btnRelatorio_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 110);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(91, 13);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Datas do Aluguel:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Dias do Aluguel:";
+            // 
+            // ckCartao
+            // 
+            this.ckCartao.AutoSize = true;
+            this.ckCartao.Enabled = false;
+            this.ckCartao.Location = new System.Drawing.Point(314, 34);
+            this.ckCartao.Name = "ckCartao";
+            this.ckCartao.Size = new System.Drawing.Size(108, 17);
+            this.ckCartao.TabIndex = 5;
+            this.ckCartao.Text = "Cartão Fidelidade";
+            this.ckCartao.UseVisualStyleBackColor = true;
+            // 
+            // lbl_nomeCliente
+            // 
+            this.lbl_nomeCliente.AutoSize = true;
+            this.lbl_nomeCliente.Location = new System.Drawing.Point(6, 35);
+            this.lbl_nomeCliente.Name = "lbl_nomeCliente";
+            this.lbl_nomeCliente.Size = new System.Drawing.Size(94, 13);
+            this.lbl_nomeCliente.TabIndex = 89;
+            this.lbl_nomeCliente.Text = "Nome do Cliente : ";
+            // 
+            // melhorCarro
+            // 
+            this.melhorCarro.FormattingEnabled = true;
+            this.melhorCarro.Location = new System.Drawing.Point(152, 185);
+            this.melhorCarro.Name = "melhorCarro";
+            this.melhorCarro.Size = new System.Drawing.Size(215, 69);
+            this.melhorCarro.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Melhor Opção de Automóvel:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(97, 35);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(0, 13);
+            this.lblNome.TabIndex = 2;
+            // 
+            // lblQtdPessoas
+            // 
+            this.lblQtdPessoas.AutoSize = true;
+            this.lblQtdPessoas.Location = new System.Drawing.Point(87, 62);
+            this.lblQtdPessoas.Name = "lblQtdPessoas";
+            this.lblQtdPessoas.Size = new System.Drawing.Size(0, 13);
+            this.lblQtdPessoas.TabIndex = 3;
+            // 
+            // lblDiasAluguel
+            // 
+            this.lblDiasAluguel.AutoSize = true;
+            this.lblDiasAluguel.Location = new System.Drawing.Point(87, 87);
+            this.lblDiasAluguel.Name = "lblDiasAluguel";
+            this.lblDiasAluguel.Size = new System.Drawing.Size(0, 13);
+            this.lblDiasAluguel.TabIndex = 4;
+            // 
+            // calculoAluguel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 296);
+            this.Controls.Add(this.lblDiasAluguel);
+            this.Controls.Add(this.lblQtdPessoas);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.melhorCarro);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.datasAluguel);
+            this.Controls.Add(this.btnRelatorio);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ckCartao);
+            this.Controls.Add(this.lbl_nomeCliente);
+            this.Controls.Add(this.txtDiretorio);
+            this.Controls.Add(this.label1);
+            this.Name = "calculoAluguel";
+            this.Text = "Calcula o Aluguel do Melhor Automóvel para o Cliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDiretorio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox datasAluguel;
+        private System.Windows.Forms.Button btnRelatorio;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox ckCartao;
+        private System.Windows.Forms.Label lbl_nomeCliente;
+        private System.Windows.Forms.ListBox melhorCarro;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblQtdPessoas;
+        private System.Windows.Forms.Label lblDiasAluguel;
+    }
+}
+
---
+++ Calculo_Aluguel/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Calculo_Aluguel
+{
+    public partial class calculoAluguel : Form
+    {
+        public calculoAluguel()
+        {
+            InitializeComponent();
+        }
+
+        string[] aluguel;
+        string[] locadora = new string[2];
+        private void txtDiretorio_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string caminho = openFileDialog1.FileName;
+
+                StreamReader x;
+
+                x = File.OpenText(openFileDialog1.FileName);
+                var qtdLinhas = File.ReadLines(openFileDialog1.FileName).Count() - 11;
+                string[] dia = new string[qtdLinhas];
+                string[] semana = new string[qtdLinhas];
+                int i = 0, diasS = 0, diasFs = 0;
+                txtDiretorio.Text = openFileDialog1.FileName;
+                string totalDias = "", qtdPessoas = "", nome = "", categoriaCliente = "";
+                Boolean locadoraS = false, locadoraW = false, locadoraN = false;
+                double valorS = 1, valorFs = 1, valorT = 0;
+
+                while (x.EndOfStream != true)
+                {
+                    string cartao = "";
+                    string linha = x.ReadLine();
+                    int pessoas = 0;
+
+                    if (linha != "")
+                    {
+                        string[] busca = linha.Split(':');
+
+                        switch (busca[0])
+                        {
+                            case "Total de dias do Aluguel":
+                                totalDias = busca[1];
+                                lblDiasAluguel.Text = totalDias;
+                                aluguel = new string[int.Parse(lblDiasAluguel.Text)];
+                                break;
+
+                            case "Cartão Fidelidade":
+                                cartao = busca[1];
+                                if (cartao == " Sim")
+                                {
+                                    ckCartao.Checked = true;
+                                    categoriaCliente = "Premium";
+                                }
+                                if (cartao == " Não")
+                                {
+                                    categoriaCliente = "Normal";
+                                }
+                                break;
+
+                            case "Nome do Cliente":
+                                nome = busca[1];
+                                lblNome.Text = nome;
+                                break;
+
+                            case "Data":
+                                dia[i] = busca[1].Substring(1, 10);
+                                semana[i] = busca[2];
+                                string teste = semana[i];
+                                if (semana[i] == " Sábado" || semana[i] == " Domingo")
+                                {
+                                    diasFs++;
+                                }
+                                else
+                                {
+                                    diasS++;
+                                }
+                                string data = dia[i] + semana[i];
+                                aluguel[i] = dia[i] + "      " + semana[i]; 
+                                datasAluguel.Items.Add(data);
+                                i++;
+                                break;
+
+                            case "Quantidade de Pessoas":
+                                qtdPessoas = busca[1];
+                                lblQtdPessoas.Text = qtdPessoas;
+                                pessoas = int.Parse(qtdPessoas);
+
+                                if (pessoas <= 2)
+                                {
+                                    locadoraW = true;
+                                }
+                                if (pessoas <= 4)
+                                {
+                                    locadoraS = true;
+                                }
+                                if (pessoas <= 7)
+                                {
+                                    locadoraN = true;
+                                }
+                                break;
+                        }
+                    }
+                }
+
+                melhorCarro.Items.Add("Categoria do Cliente: " + categoriaCliente);
+                melhorCarro.Items.Add("");
+                int y = 0;
+                switch (categoriaCliente)
+                {
+                    case "Premium":
+                        if (locadoraS)
+                        {
+                            valorFs = diasFs * 90;
+                            valorS = diasS * 150;
+                            valorT = valorS + valorFs;
+                            melhorCarro.Items.Add("Locadora: SouthCar");
+                            melhorCarro.Items.Add("Valor total do Aluguel: " + valorT.ToString());
+                            melhorCarro.Items.Add("Tipo do Automóvel: Compactos");
+                            melhorCarro.Items.Add("");
+                        }
+                        if (locadoraW)
+                        {
+                            valorFs = diasFs * 90;
+                            valorS = diasS * 150;
+                            valorT = valorS + valorFs;
+                            melhorCarro.Items.Add("Locadora: WestCar");
+                            melhorCarro.Items.Add("Valor total do Aluguel: " + valorT.ToString());
+                            melhorCarro.Items.Add("Tipo do Automóvel: Esportivos");
+                            melhorCarro.Items.Add("");
+                        }
+                        if (locadoraN)
+                        {
+                            valorFs = diasFs * 590;
+                            valorS = diasS * 580;
+                            valorT = valorS + valorFs;
+                            melhorCarro.Items.Add("Locadora: NorthCar");
+                            melhorCarro.Items.Add("Valor total do Aluguel: " + valorT.ToString());
+                            melhorCarro.Items.Add("Tipo do Automóvel: SUVs");
+                            melhorCarro.Items.Add("");
+                        }
+                        y++;
+                        break;
+
+                    case "Normal":
+                        if (locadoraS)
+                        {
+                            valorFs = diasFs * 200;
+                            valorS = diasS * 210;
+                            valorT = valorS + valorFs;
+                            melhorCarro.Items.Add("Locadora: SouthCar");
+                            melhorCarro.Items.Add("Valor total do Aluguel: " + valorT.ToString());
+                            melhorCarro.Items.Add("Tipo do Automóvel: Compactos" );
+                            melhorCarro.Items.Add("");
+                        }
+                        if (locadoraW)
+                        {
+                            valorFs = diasFs * 200;
+                            valorS = diasS * 530;
+                            valorT = valorS + valorFs;
+                            melhorCarro.Items.Add("Locadora: WestCar");
+                            melhorCarro.Items.Add("Valor total do Aluguel: " + valorT.ToString());
+                            melhorCarro.Items.Add("Tipo do Automóvel: Esportivos");
+                            melhorCarro.Items.Add("");
+                        }
+                        if (locadoraN)
+                        {
+                            valorFs = diasFs * 600;
+                            valorS = diasS * 630;
+                            valorT = valorS + valorFs;
+                            melhorCarro.Items.Add("Locadora: NorthCar");
+                            melhorCarro.Items.Add("Valor total do Aluguel: " + valorT.ToString());
+                            melhorCarro.Items.Add("Tipo do Automóvel: SUVs");
+                            melhorCarro.Items.Add("");
+                        }
+                        y++;
+                        break;
+
+                }
+
+                x.Close();
+            }
+        }
+
+        private void btnRelatorio_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "Selecione a pasta onde será criado o arquivo:";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamWriter x;
+
+                x = File.CreateText(System.IO.Path.Combine(dialog.SelectedPath, "Proposta de Aluguel do Cliente " + lblNome.Text + ".txt"));
+
+                x.WriteLine("Proposta de Aluguel do Cliente" + lblNome.Text);
+                x.WriteLine();
+                x.WriteLine("Nome do Cliente: " + lblNome.Text);
+                x.WriteLine("===========================================");
+                if (ckCartao.Checked == true)
+                {
+                    x.WriteLine("Cartão Fidelidade: Sim");
+                }
+                else
+                {
+                    x.WriteLine("Cartão Fidelidade: Não");
+                }
+                x.WriteLine("===========================================");
+                x.WriteLine("Quantidade de Pessoas: " + lblQtdPessoas.Text);
+                x.WriteLine("===========================================");
+                x.WriteLine("Total de dias do Aluguel: " + lblDiasAluguel.Text);
+                x.WriteLine("===========================================");
+                x.WriteLine("Dias do Aluguel  Dia da Semana do Aluguel");
+                for (int i = 0; i < int.Parse(lblDiasAluguel.Text); i++)
+                {
+                    x.WriteLine(aluguel[i]);
+                }
+                x.WriteLine("===========================================");
+                int vt = melhorCarro.Items.Count;
+                for(int i = 0; i < vt; i++)
+                {
+                    var teste = melhorCarro.Items[i];
+                    if(teste == "" && i < 2)
+                    {
+                        x.WriteLine("===========================================");
+                    }
+                    x.WriteLine(teste);
+                }
+
+                x.Close();
+
+                Application.Exit();
+            }
+        }
+    }
+}
+
+
